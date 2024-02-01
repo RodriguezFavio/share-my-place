62af02846cf4732306e304687d9b7ed9fadf5a85
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-01-31 22:18:07 -0300

    I corrected a syntax error and defined the findAddressHandler function

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,18 +1,19 @@
 import { Modal } from "./UI/Modal";
 import { Map } from "./UI/Map";
+import { getCoordsFromAddress } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
-    const addresForm = document.querySelector("form");
+    const addressForm = document.querySelector("form");
     const locateUserBtn = document.getElementById("locate-btn");
 
     locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
-    addresForm.addEventListener("click", this.findAddressHandler.bind(this));
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
   }
 
   selectPlace(coordinates) {
     if (this.map) {
-      this.map.render();
+      this.map.render(coordinates);
     } else {
       this.map = new Map(coordinates);
     }
@@ -50,7 +51,27 @@ class PlaceFinder {
     );
   }
 
-  findAddressHandler() {}
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again!");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates);
+    } catch (error) {
+      alert(error.message);
+    }
+    modal.hide();
+  }
 }
 
 const placeFinder = new PlaceFinder();
