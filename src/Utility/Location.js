b7df2a4e0f2c5c956630d7db9de968ea73d24a47
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-02-05 20:31:06 -0300

    created a new function to obtain the address using the coordinates

--- src/Utility/Location.js
+++ src/Utility/Location.js
@@ -1,5 +1,22 @@
 const GOOGLE_API_KEY = "AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik";
 
+export const getAddresFromCoords = async (coords) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch address. Please try again!");
+  }
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+
+  const address = data.results[0].formatted_address;
+  return address;
+};
+
 export const getCoordsFromAddress = async (address) => {
   const urlAddress = encodeURI(address);
   const response = await fetch(
@@ -13,6 +30,6 @@ export const getCoordsFromAddress = async (address) => {
     throw new Error(data.error_message);
   }
 
-  const coordinates = data.result[0].geometry.location;
+  const coordinates = data.results[0].geometry.location;
   return coordinates;
 };
